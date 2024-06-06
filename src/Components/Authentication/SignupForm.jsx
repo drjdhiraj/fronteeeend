2382842264ad1ea4cb78bba25f5b1d09b0761889
Dhiraj Raj <dhirajraj7227@gmail.com>
2024-06-07 04:17:04 +0530

    Update SignupForm.jsx

--- src/Components/Authentication/SignupForm.jsx
+++ src/Components/Authentication/SignupForm.jsx
@@ -21,11 +21,22 @@ const validationSchema = Yup.object().shape({
 });
 
 const days = Array.from({ length: 31 }, (_, i) => i + 1);
+
 const months = [
   { value: 1, label: "January" },
   { value: 2, label: "February" },
-  // Add other months here
+  { value: 3, label: "March" },
+  { value: 4, label: "April" },
+  { value: 5, label: "May" },
+  { value: 6, label: "June" },
+  { value: 7, label: "July" },
+  { value: 8, label: "August" },
+  { value: 9, label: "September" },
+  { value: 10, label: "October" },
+  { value: 11, label: "November" },
+  { value: 12, label: "December" },
 ];
+
 const currentYear = new Date().getFullYear();
 const years = Array.from({ length: 100 }, (_, i) => currentYear - i);
 
