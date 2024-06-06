2b691a28430eef948d8894b39e3c3e344995a0e5
Dhiraj Raj <dhirajraj7227@gmail.com>
2024-06-07 01:42:03 +0530

    Update PostCountCard.jsx

--- src/Admin/PostCountCard.jsx
+++ src/Admin/PostCountCard.jsx
@@ -6,7 +6,7 @@ const PostCountCard = () => {
   const [postCount, setPostCount] = useState(0);
 
   useEffect(() => {
-    axios.get('https://technoblogs.azurewebsites.net')
+    axios.get('http://localhost:5454/postcount')
       .then(response => {
         setPostCount(response.data);
       })
