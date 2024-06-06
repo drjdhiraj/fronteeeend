cd54aab4821ac9c44503569593ded00a9a14bfcf
Dhiraj Raj <dhirajraj7227@gmail.com>
2024-06-07 01:28:00 +0530

    Update apiConfig.js

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const jwtToken = localStorage.getItem("jwt")
-export const API_BASE_URL = 'http://localhost:5454/getAllUsers';
+export const API_BASE_URL = 'http://localhost:5454';
 export const api = axios.create({
   baseURL: API_BASE_URL, 
   headers: {
